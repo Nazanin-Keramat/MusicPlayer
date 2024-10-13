064de140492ed48fbb36b1f7f5e2b8a00c0d367b
NazaninKeramat <Keramatnazanin.id@gmail.com>
2024-10-13 19:16:52 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,264 @@
+// this array contains musics
+let musics = [
+  {
+    name: "I Forgive You",
+    singer: "Sia",
+    cover: "./media/Sia - I Forgive You.jpg",
+    audio: new Audio("./music/Sia - I Forgive You.mp3"),
+    lyric:
+      "I felt the life, felt the life Slip out of me Black as night, as nightBlue as the sea, yeahI would have done anything Would have given everything for you I would have done anything Would have given everything for you I-I-I, I forgive you, you know not what you have done Oh, I-I, I forgive you, now it's time for me to move on Oh, I-I, I forgive you, you could not see right from wrong Oh, I, and I love you always in my heart, you'll live on You'll live on Good people sin when yearning's Unspoken-en, ooh You broke my heart, broke my heart But it's still beating Oh, I would have done anything Would have given everything for you I would have done anything Would have given everything for you I-I-I, I forgive you, you know not what you have done Oh, I-I, I forgive you, now it's time for me to move on Oh, I-I, I forgive you, you could not see right from wrong Oh, I, and I love you always in my heart, you'll live on You'll live on",
+    index: "0",
+  },
+  {
+    name: "Show Yourself",
+    singer: "idina Menzel",
+    cover: "./media/Show yourself _ idina Menzel.jpeg",
+    audio: new Audio("./music/idina_menzel-show_yourself_gratomic.com_.mp3"),
+    lyric:
+      "Every inch of me is trembling But not from the cold Something is familiar Like a dream I can reach but not quite hold I can sense you there Like a friend I've always known I'm arriving And it feels like I am home I have always been a fortress Cold secrets deep inside You have secrets, too But you don't have to hide Show yourself I'm dying to meet you Show yourself It's your turn Are you the one I've been looking for All of my life? Show yourself I'm ready to learn Ah-ah, ah-ah Ah-ah, ah-ah-ah I've never felt so certain All my life I've been torn But I'm here for a reason Could it be the reason I was born? I have always been so different Normal rules did not apply Is this the day? Are you the way I finally find out why? Show yourself I'm no longer trembling Here I am I've come so far You are the answer I've waited for All of my life Oh, show yourself Let me see who you are Come to me now Open your door Don't make me wait One moment more Oh, come to me now Open your door Don't make me wait One moment more Where the north wind meets the sea Ah-ah, ah-ah There's a river Ah-ah, ah-ah Full of memory Come, my darling, homeward bound I am found Show yourself Step into your power Grow yourself Into something new You are the one you've been waiting for All of my life All of your life Oh, show yourself Ah-ah, ah-ah (ah-ah, ah-ah) Ah-ah, ah-ah (ah-ah, ah-ah) Ah-ah, ah-ah",
+    index: "1",
+  },
+  {
+    name: "Diamond",
+    singer: "rihanna",
+    cover: "./media/rihanna-accordi.jpg",
+    audio: new Audio("./music/diamonds.mp3"),
+    lyric:
+      "Shine bright like a diamond Shine bright like a diamond Find light in the beautiful sea, I choose to be happy You and I, you and I, we're like diamonds in the sky You're a shooting star I see, a vision of ecstasy When you hold me, I'm alive, we're like diamonds in the sky I knew that we'd become one right away Oh, right away At first sight I felt the energy of sun rays I saw the life inside your eyes So shine bright, tonight, you and I We're beautiful like diamonds in the sky Eye to eye, so alive We're beautiful like diamonds in the sky Shine bright like a diamond (whoa) Shine bright like a diamond (whoa) Shining bright like a diamond We're beautiful like diamonds in the sky Shine bright like a diamond (whoa) Shine bright like a diamond (whoa) Shining bright like a diamond We're beautiful like diamonds in the sky Palms rise to the universe as we moonshine and molly Feel the warmth, we'll never die, we're like diamonds in the sky You're a shooting star I see, a vision of ecstasy When you hold me, I'm alive, we're like diamonds in the sky At first sight I felt the energy of sun rays I saw the life inside your eyes So shine bright, tonight, you and I We're beautiful like diamonds in the sky Eye to eye, so alive We're beautiful like diamonds in the sky Shine bright like a diamond (whoa) Shine bright like a diamond (whoa) Shining bright like a diamond We're beautiful like diamonds in the sky Shine bright like a diamond (whoa) Shine bright like a diamond (whoa) Shining bright like a diamond We're beautiful like diamonds in the sky Shine bright like a diamond Shine bright like a diamond Shine bright like a diamond So shine bright, tonight, you and I We're beautiful like diamonds in the sky Eye to eye, so alive We're beautiful like diamonds in the sky Shine bright like a diamond (whoa) Shine bright like a diamond (whoa) Shine bright like a diamond, oh-oh, yeah Shine bright like a diamond (whoa) Shine bright like a diamond (whoa) Shine bright like a diamond Shine bright like a diamond",
+    index: "2",
+  },
+  {
+    name: "Calm Down",
+    cover: "./media/calmdown.jpg",
+    audio: new Audio("./music/rema-selena-gomez-calm-down-320.mp3"),
+    lyric:
+      "Baby, calm down, calm down Girl, this your body e put my heart for lockdown For lockdown, oh, lockdown Girl, you sweet like Fanta, Fanta If I tell you say I love you no dey form yanga, oh, yanga No, tell me no, no, no, no, whoa-whoa, whoa-whoa Oh-oh-oh-oh-oh-oh-oh-oh-oh-oh-oh Baby, come gimme your lo-lo-lo-lo-lo-lo-lo-lo, whoa-whoa-whoa-whoa-whoa You got me like whoa-whoa-whoa-whoa-whoa-whoa-whoa-whoa-whoa Shawty, come gimme your lo-lo-lo-lo-lo-lo-lo-lo-whoa-whoa-whoa-whoa-whoa, hmm I see this fine girl for my party, she wear yellow Every other girl they dey do too much, but this girl mellow Naim I dey find situation I go use take tell am hello Finally I find way to talk to the girl but she no wan follow Who you come dey form for? (Mm-hm) Why you no wan comform? (Mm-hm) Then I start to feel her bum-bum (mm-hm) But she dey gimme small-small I know say she sabi pass that one (mm-hm) But she feeling insecure 'Cause her friends go dey gum her like chewing gum Go dey gum her like chewing gum Baby, calm down, calm down Girl, this your body e put my heart for lockdown For lockdown, oh, lockdown Girl, you sweet like Fanta, Fanta If I tell you say I love you no dey form yanga, oh, yanga No, tell me no, no, no, no, whoa-whoa, whoa-whoa Oh-oh-oh-oh-oh-oh-oh-oh-oh-oh-oh Baby, come gimme your lo-lo-lo-lo-lo-lo-lo-lo, whoa-whoa-whoa-whoa-whoa You got me like whoa-whoa-whoa-whoa-whoa-whoa-whoa-whoa-whoa Shawty, come gimme your lo-lo-lo-lo-lo-lo-lo-lo-whoa-whoa-whoa-whoa-whoa, hmm As I reach my house, I say make I rest small (make I rest small) As me I wake up na she dey my mind (na she dey my mind) Day one, day two, I no fit focus (I no fit focus) Na so me I call am, say make we link up (say make we link up) As I start to dey tell her how I feel, na my heart dey race Baby girl, if you leave me I no go love again Because e get many girls wey put my heart for pain Shebi you feel my pain? Baby, calm down, calm down Girl, this your body e put my heart for lockdown For lockdown, oh, lockdown Girl, you sweet like Fanta, Fanta If I tell you say I love you no dey form yanga, oh, yanga No, tell me no, no, no, no, whoa-whoa, whoa-whoa Oh-oh-oh-oh-oh-oh-oh-oh-oh-oh-oh Baby, come gimme your lo-lo-lo-lo-lo-lo-lo-lo, whoa-whoa-whoa-whoa-whoa You got me like whoa-whoa-whoa-whoa-whoa-whoa-whoa-whoa-whoa Shawty, come gimme your lo-lo-lo-lo-lo-lo-lo-lo-whoa-whoa-whoa-whoa-whoa, hmm",
+    index: "3",
+  },
+];
+
+// selection
+let range = document.querySelector("#music-time");
+let playBtn = document.querySelector("#play-btn");
+let nextBtn = document.querySelector("#next-btn");
+let preBtn = document.querySelector("#pre-btn");
+let musicCover = document.querySelector("#music-cover");
+let musicName = document.querySelector("#music-name");
+// set current music
+let currentMusic = 0;
+// extract audio
+let audio = musics[currentMusic].audio;
+// setting name music and img for cover
+musicCover.src = musics[currentMusic].cover;
+musicName.innerText = musics[currentMusic].name;
+
+// Has music loaded?
+audio.addEventListener("canplay", () => {
+  range.max = audio.duration;
+  console.log(audio.duration);
+});
+
+// the range bar can trck music
+audio.addEventListener("timeupdate", () => {
+  range.value = audio.currentTime;
+});
+
+// when user click on range bar the music go in that time
+range.addEventListener("input", () => {
+  audio.currentTime = range.value;
+});
+
+//play and pause handelling
+playBtn.addEventListener("click", () => {
+  //property paused check play and pause also print true or false
+  if (audio.paused) {
+    audio.play();
+    musicCover.style.animationPlayState = "running";
+    playBtn.classList.replace("fa-play", "fa-pause");
+  } else {
+    audio.pause();
+    musicCover.style.animationPlayState = "paused";
+    playBtn.classList.replace("fa-pause", "fa-play");
+  }
+});
+
+// next and previous music
+nextBtn.addEventListener("click", () => {
+  // define function if we give next will go next music
+  changeMusic("next");
+});
+preBtn.addEventListener("click", () => {
+  // define function if we give next will go previous music
+  changeMusic("pre");
+});
+
+// when we want to change music firstly should pause after that become at the first of range bar then  our icon should be change and our animation should paused and should make current time 0 after that we say if our entrace is next do this unless do this
+function changeMusic(state) {
+  audio.pause();
+  range.value = 0;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  musicCover.style.animationPlayState = "paused";
+  audio.currentMusic = 0;
+
+  if (state == "next") {
+    if (currentMusic == musics.length - 1) currentMusic = 0;
+    else currentMusic += 1;
+  } else {
+    if (currentMusic == 0) currentMusic = musics.length - 1;
+    else currentMusic -= 1;
+  }
+
+  audio = musics[currentMusic].audio;
+  // setting name music and img for cover
+  musicCover.src = musics[currentMusic].cover;
+  musicName.innerText = musics[currentMusic].name;
+  audio.addEventListener("canplay", () => {
+    range.max = audio.duration;
+    console.log(audio.duration);
+  });
+  audio.addEventListener("timeupdate", () => {
+    range.value = audio.currentTime;
+  });
+}
+
+// what happen after end of music-i want to say play next music
+function endMusic() {
+  let inputRange = document.getElementById("music-time");
+  if (inputRange.value == Math.floor(audio.duration)) {
+    audio.pause();
+    changeMusic("next");
+  }
+}
+setInterval(endMusic, 1000);
+// handel lyrics
+document.getElementById("myButton").addEventListener("click", lyricFunc);
+let button = document.getElementById("myButton");
+let x = document.getElementById("myDiv");
+let h2 = document.querySelector("#lyric-title");
+let p = document.querySelector("#lyric");
+h2.innerText = musics[currentMusic].name;
+p.innerText = musics[currentMusic].lyric;
+button.innerText = "Hide Lyric";
+
+// toggle button for showing the lyrics
+function lyricFunc() {
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    button.innerText = "Hide Lyric";
+    h2.innerText = musics[currentMusic].name;
+    p.innerText = musics[currentMusic].lyric;
+  } else {
+    x.style.display = "none";
+    button.innerText = "Show Lyric";
+  }
+}
+
+// handel playlist section
+let saveCur = currentMusic;
+
+for (let i = 0; i < musics.length; i++) {
+  currentMusic = i;
+  let vari0 = document.getElementById("playList");
+
+  let vari2 = document.createElement("div");
+  vari2.id = "playListDiv";
+  vari2.className = i;
+  let vari3 = document.createElement("div");
+  vari3.id = "rounded";
+  let vari4 = document.createElement("img");
+  vari4.id = "imagePlayList";
+  vari4.src = musics[currentMusic].cover;
+  let vari5 = document.createElement("div");
+  vari5.id = "Titles";
+
+  let vari6 = document.createElement("h3");
+  vari6.id = "titleMusicName";
+  vari6.className = "V" + musics[currentMusic].index;
+  vari6.innerText = musics[currentMusic].name;
+
+  vari0.append(vari2, vari5);
+  vari2.append(vari3, vari4);
+  vari2.appendChild(vari6);
+  vari6.appendChild(vari5);
+  vari3.appendChild(vari4);
+
+  // document.getElementById("playList").innerHTML =
+  //   `
+  //     <h2 id="playListTitle">PlayList</h2>
+  //     <div id="playListDiv" >
+  //         <div id="rounded">
+  //         <img id="imagePlayList" src="./media/calmdown.jpg">
+  //         </div>
+  //         <div id="Titles">
+  //         <h3 id="titleMusicName">Song : </h3>
+  //         <h3 id="playListName">clam</h3>
+  //
+  //         </div>
+  // `;
+
+  // document.getElementById("playListName").innerText =musics[currentMusic].name;
+  // document.getElementById("imagePlayList").src = musics[currentMusic].cover;
+}
+// handel current music
+// setInterval(CurrentPlayed, 1000);
+// function CurrentPlayed(params) {
+//   // let change0 = document.querySelectorAll("#titleMusicName")[saveCur];
+//   // change0.parentElement.className = "played";
+
+//   function CurrentPlayedShow() {
+//     changeMusic("next");
+//     let change0 = document.querySelectorAll("#titleMusicName")[musics[currentMusic].index];
+//     change0.parentElement.className = "played";
+//     console.log(currentMusic);
+//   }
+// //   if (saveCur++) {
+// //     changeMusic("next");
+// //     let change0 = document.querySelectorAll("#titleMusicName")[saveCur];
+// //     change0.parentElement.className = "played";
+// //     console.log(saveCur);
+
+// //   }
+// }
+// let indexAudio = musics[saveCur].index;
+
+// document.querySelector(".next-btn").addEventListener("click", CurrentPlayed);
+// console.log(document.querySelectorAll("#titleMusicName")[saveCur]);
+document.getElementById("playList").addEventListener("click", FuncH);
+
+function FuncH(event) {
+  console.log(event.target.className);
+  if (event.target.id === "playListDiv") {
+    currentMusic = event.target.className;
+    audio.pause();
+    musicCover.style.animationPlayState = "paused";
+    playBtn.classList.replace("fa-pause", "fa-play");
+    let change0 =
+      document.querySelectorAll("#titleMusicName")[musics[currentMusic].index];
+    console.log(
+      document.querySelectorAll("#titleMusicName")[musics[currentMusic].index]
+    );
+    change0.parentElement.className = "played";
+    audio = musics[musics[currentMusic].index].audio;
+    // setting name music and img for cover
+    musicCover.src = musics[currentMusic].cover;
+    musicName.innerText = musics[currentMusic].name;
+
+    // Has music loaded?
+    audio.addEventListener("canplay", () => {
+      range.max = audio.duration;
+      console.log(audio.duration);
+    });
+
+    // the range bar can trck music
+    audio.addEventListener("timeupdate", () => {
+      range.value = audio.currentTime;
+    });
+
+    // when user click on range bar the music go in that time
+    range.addEventListener("input", () => {
+      audio.currentTime = range.value;
+    });
+    console.log(change0.parentElement);
+  }
+  currentMusic = saveCur;
+}
